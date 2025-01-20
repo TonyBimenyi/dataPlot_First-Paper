3dfb68521b60c9fe924185f827d92184ebb874c7
TonyBimenyi <tonyblaisb@gmail.com>
2025-01-20 21:51:35 +0800

    adding markersize

--- test.py
+++ test.py
@@ -17,20 +17,27 @@ with open(file_path, 'r') as file:
 
 ch3_data_match = re.search(r'CH3_Data_OutPut\[\d+\]=\{(.*?)\};',  file_content,re.DOTALL)
 ch1_data_match = re.search(r'CH1_Data_OutPut\[\d+\]=\{(.*?)\};',  file_content,re.DOTALL)
+ch2_data_match = re.search(r'CH2_Data_OutPut\[\d+\]=\{(.*?)\};',  file_content,re.DOTALL)
 
-if ch3_data_match and ch1_data_match:
+if ch3_data_match and ch1_data_match and ch2_data_match:
     #Convert the extracted data to a list of floats
     ch3_data = list(map(float, ch3_data_match.group(1).split(',')))
     ch1_data = list(map(float, ch1_data_match.group(1).split(',')))
+    ch2_data = list(map(float, ch2_data_match.group(1).split(',')))
 
      # Plot the data
     plt.figure(figsize=(10, 6))
-    plt.plot(ch3_data, label='CH3_Data_OutPut[1707]', color='blue')
+    plt.plot(ch1_data, '-*', label='CH2_Data_OutPut[1707]', color='green', markersize=6)
+    plt.plot(ch3_data,'-', label='CH3_Data_OutPut[1707]', color='blue', markersize=2)
     plt.plot(ch1_data, label='CH1_Data_OutPut[1707]', color='red')
-    plt.title('CH3_Data_OutPut[1707] Plot')
+
+    plt.title('CH3_Data_OutPut[1707] Plot', fontweight='bold')
     plt.xlabel('Index')
     plt.ylabel('Value')
-    plt.grid()
+    # Adding multiple grids
+    plt.grid(True, which='major', color='black', linestyle='-', linewidth=0.85)  # Major grid
+    plt.grid(True, which='minor', color='gray', linestyle='--', linewidth=0.5)  # Minor grid
+    plt.minorticks_on()  # Enable minor ticks for better detai
     plt.legend()
     plt.show()
 else:
