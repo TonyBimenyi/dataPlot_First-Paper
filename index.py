4aad916179b25c8c5baf945d18184625ac13c5c2
TonyBimenyi <tonyblaisb@gmail.com>
2025-01-20 21:07:24 +0800

    plot data

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 # Define parameters
 
@@ -10,3 +11,23 @@ file_path = 'datascope.txt'  # Replace with your file's path
 
 with open(file_path, 'r') as file:
     file_content = file.read()
+
+
+#Extract the CH3 data
+
+ch3_data_match = re.search(r'CH3_Data_OutPut\[\d+\]=\{(.*?)\};',  file_content,re.DOTALL)
+if ch3_data_match:
+    #Convert the extracted data to a list of floats
+    ch3_data = list(map(float, ch3_data_match.group(1).split(',')))
+
+     # Plot the data
+    plt.figure(figsize=(10, 6))
+    plt.plot(ch3_data, label='CH3_Data_OutPut[1707]', color='blue')
+    plt.title('CH3_Data_OutPut[1707] Plot')
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.grid()
+    plt.legend()
+    plt.show()
+else:
+    print("CH3_Data_OutPut[1707] not found in the file.")
