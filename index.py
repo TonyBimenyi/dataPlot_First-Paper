06f7e96762522f47d3ab35eedc4521eb4dff27df
TonyBimenyi <tonyblaisb@gmail.com>
2025-01-20 21:01:37 +0800

    load te content

--- index.py
+++ index.py
@@ -2,3 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define parameters
+
+# Path to the file
+file_path = 'datascope.txt'  # Replace with your file's path
+
+#Load the content of the data
+
+with open(file_path, 'r') as file:
+    file_content = file.read()
