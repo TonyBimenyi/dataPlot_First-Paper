8894c4788d02335cc1fadc3df10cc36a594abc1e
TonyBimenyi <tonyblaisb@gmail.com>
2025-01-20 21:23:43 +0800

    adding ch2

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ with open(file_path, 'r') as file:
 ch3_data_match = re.search(r'CH3_Data_OutPut\[\d+\]=\{(.*?)\};',  file_content,re.DOTALL)
 ch1_data_match = re.search(r'CH1_Data_OutPut\[\d+\]=\{(.*?)\};',  file_content,re.DOTALL)
 
-if ch3_data_match & ch1_data_match:
+if ch3_data_match and ch1_data_match:
     #Convert the extracted data to a list of floats
     ch3_data = list(map(float, ch3_data_match.group(1).split(',')))
     ch1_data = list(map(float, ch1_data_match.group(1).split(',')))
@@ -26,6 +26,7 @@ if ch3_data_match & ch1_data_match:
      # Plot the data
     plt.figure(figsize=(10, 6))
     plt.plot(ch3_data, label='CH3_Data_OutPut[1707]', color='blue')
+    plt.plot(ch1_data, label='CH1_Data_OutPut[1707]', color='red')
     plt.title('CH3_Data_OutPut[1707] Plot')
     plt.xlabel('Index')
     plt.ylabel('Value')
